8a7a2e9b6151f9ef91a7494f06a86d4656aa52f7
Felipe Laskos <felipelaskosmatheus@gmail.com>
2023-02-03 21:55:07 -0300

    transformando PlayersName em componente

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 import Title from './components/Title';
 import Teams from "./components/Teams";
 import Game from './components/Game';
-import { FaUndoAlt, FaRegCircle, FaTimes } from 'react-icons/fa';
+import PlayersName from "./components/PlayersName";
+import { FaUndoAlt } from 'react-icons/fa';
 
 function VerifyWinner(array) {
   const possibilitysWin = [
@@ -120,36 +121,7 @@ function App() {
       <div className="restart-icon_container" onClick={restartGame}>
         <FaUndoAlt className="restart-icon" title="Restart Game" />
       </div>
-      <div className="player-names_container">
-        <div className='label-input'>
-          {!playerTeam && (
-            <label htmlFor="player1">P1</label>
-          )}
-          {playerTeam === "X" && (
-            <FaTimes className='icon-player blue' />
-          )}
-          {playerTeam === "O" && (
-            <FaRegCircle className='icon-player red' />
-          )}
-          <input className="player-name" value={playersName[0]} id="player1" maxLength={10} onChange={(event) => {
-            handleInputPlayer1Change(event.target.value);
-          }} />
-        </div>
-        <div className='label-input'>
-          {!playerTeam && (
-            <label htmlFor="player2">P2</label>
-          )}
-          {playerTeam === "X" && (
-            <FaRegCircle className='icon-player red' />
-            )}
-          {playerTeam === "O" && (
-            <FaTimes className='icon-player blue' />
-          )}
-          <input className="player-name" value={playersName[1]} id="player2" maxLength={10} onChange={(event) => {
-            handleInputPlayer2Change(event.target.value);
-          }} />
-        </div>
-      </div>
+      <PlayersName playerTeam={playerTeam} playersName={playersName} handleInputPlayer1Change={handleInputPlayer1Change} handleInputPlayer2Change={handleInputPlayer2Change} />
     </div>
   );
 }
---
+++ src/components/PlayersName/index.js
@@ -0,0 +1,39 @@
+import { FaRegCircle, FaTimes } from 'react-icons/fa';
+import "./style.css";
+
+const PlayersName = ({ playerTeam, playersName, handleInputPlayer1Change, handleInputPlayer2Change }) => {
+    return (
+        <div className="player-names_container">
+            <div className='label-input'>
+            {!playerTeam && (
+                <label htmlFor="player1">P1</label>
+            )}
+            {playerTeam === "X" && (
+                <FaTimes className='icon-player blue' />
+            )}
+            {playerTeam === "O" && (
+                <FaRegCircle className='icon-player red' />
+            )}
+            <input className="player-name" value={playersName[0]} id="player1" maxLength={10} onChange={(event) => {
+                handleInputPlayer1Change(event.target.value);
+            }} />
+            </div>
+            <div className='label-input'>
+            {!playerTeam && (
+                <label htmlFor="player2">P2</label>
+            )}
+            {playerTeam === "X" && (
+                <FaRegCircle className='icon-player red' />
+                )}
+            {playerTeam === "O" && (
+                <FaTimes className='icon-player blue' />
+            )}
+            <input className="player-name" value={playersName[1]} id="player2" maxLength={10} onChange={(event) => {
+                handleInputPlayer2Change(event.target.value);
+            }} />
+            </div>
+        </div>
+    );
+};
+
+export default PlayersName;
